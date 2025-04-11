e677a9354b3c80aa439b1118c35388ddd5a1e94d
Matheus <matheus8142@hotmail.com>
2025-04-11 16:41:57 -0300

    Importando bibliotecas
    *jQuery
    *Bootstrap

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Controle de Garagem</title>
+</head>
+<body>
+
+    <div class="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
+    </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
